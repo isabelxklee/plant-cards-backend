87c2cda53e4d868757971738348465c3249536fd
isabel k. lee <hello@isabelklee.co>
2020-05-06 20:45:59 -0400

    updated players controller and routes

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,18 +1,27 @@
 class PlayersController < ApplicationController
   def index 
-    players = Player.all
-    render json: players
+    @players = Player.all
+    render json: @players
   end
 
   def show
-    player = Player.find(params[:id])
-    render json: player
+    @player = Player.find(params[:id])
+    render json: @player
   end
 
   def new 
+    @player = Player.new
   end 
 
   def create 
+    @player = Player.create(player_params)
+
+    respond_to do |format|
+      format.html { redirect_to @player, notice: 'Player was successfully created.' }
+      format.json { render json: @player }
+      # format.json { render :show, status: :created, location: @player }
+    end
+
   end 
 
   def show
@@ -26,4 +35,13 @@ class PlayersController < ApplicationController
 
   def destroy
   end
+
+  private
+    def set_player
+      @player = Player.find(params[:id])
+    end
+
+    def player_params
+      params.require(:player).permit(:username, :highscore)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   resources :plants, only: [:index, :show]
   resources :facts, only: [:index, :show]
   resources :flashcards, only: [:index, :show]
-  resources :players, only: [:index, :new, :show, :edit, :destroy]
   resources :questions, only: [:index, :show]
+
+  get '/players', to: 'players#index'
+  post '/players/', to: 'players#create'
+  get '/players/:id', to: 'players#show', as: 'player'
+  get '/players/:id/edit', to: 'players#edit', as: 'edit_player'
+  patch '/players/:id', to: 'players#update'
+  delete 'players/:id', to: "players#destroy"
 end
