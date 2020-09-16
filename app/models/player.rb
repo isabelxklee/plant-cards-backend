092e5866f6c0a8510c9b6f5bd08ac3bc830b890d
isabel k. lee <hello@isabelklee.co>
2020-09-16 15:55:08 -0400

    updated player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ApplicationRecord
     validates :username, presence: true
     validates :username, length: { maximum: 30 }
-    validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/, message: "Only allows letters and numbers." }
+    # validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/, message: "Only allows letters and numbers." }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 Plant.destroy_all
 Fact.destroy_all
 Flashcard.destroy_all
-Player.destroy_all
+# Player.destroy_all
 Question.destroy_all
 Answer.destroy_all
 
 ## players ##
-Player.create(username: "plantdestroyer", highscore: 300)
-Player.create(username: "finkiecat", highscore: 150)
-Player.create(username: "isabel", highscore: 50)
-Player.create(username: "plantlov3r", highscore: 250)
+# Player.create(username: "plantdestroyer", highscore: 300)
+# Player.create(username: "finkiecat", highscore: 150)
+# Player.create(username: "isabel", highscore: 50)
+# Player.create(username: "plantlov3r", highscore: 250)
 
 ## plants ##
 snake_plant = Plant.create(name: "Snake Plant", image: "https://i.imgur.com/oCgg11o.png")
